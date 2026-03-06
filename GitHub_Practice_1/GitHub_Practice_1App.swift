a8a01a39843fb4455395b7c200ac126fd5ecc6c6
Imran Khan <imktk87@gmail.com>
2026-03-07 02:41:02 +0500

    Intial Commit

---
+++ GitHub_Practice_1/GitHub_Practice_1App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_Practice_1App.swift
+//  GitHub_Practice_1
+//
+//  Created by Imran2 on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_Practice_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
